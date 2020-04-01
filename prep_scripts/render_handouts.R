85028c04b72689b014c4d1984f98fd26cf075555
camille-s <camille.seaberry@gmail.com>
2020-03-31 22:25:46 -0400

    Add rendered handouts

--- prep_scripts/render_handouts.R
+++ prep_scripts/render_handouts.R
@@ -10,21 +10,9 @@ ids <- read_csv("_utils/legislative_to_town_lookup.csv") %>%
 ids %>%
   imap(function(id_list, chamber) {
     walk(id_list, ~rmarkdown::render(input = "./prep_scripts/param_handout.Rmd",
-                                     output_file = str_glue("handouts/{chamber}/{acs_year}_profile_{.}.pdf"),
+                                     output_dir = file.path("handouts", chamber),
+                                     output_file = str_glue("{acs_year}_profile_{.}.pdf"),
                                      params = list(geoid = .)))
   })
 
-# ids <- read_csv("../town_x_district.csv") %>%
-#   select(-town) %>%
-#   unique() %>%
-#   mutate(num = stringr::str_sub(id, -3)) %>%
-#   mutate(name = paste(chamber, num, sep = "_")) %>%
-#   filter(chamber == "Senate")
-
-# walk2(ids$id, ids$name,  ~rmarkdown::render(input = "./legislative_handout_v2.Rmd", output_file = sprintf("./output/2016_profile_%s.pdf", .y), params = list(this_id = .x)))
-
-# walk2(ids$id, ids$name, function(id, name) {
-#   rmarkdown::render(input = "./legislative_handout_v2.Rmd", output_file = sprintf("./output/2016_profile_%s.pdf", name), params = list(this_id = id))
-#   dev.off()
-#   print(id)
-# })
+BRRR::skrrrahh(18)
